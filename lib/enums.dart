e8cc7d26f3e34e54cd293f037a4fbf7e7ef11788
Ahmed2007AlHossery <137702713+Ahmed2007AlHossery@users.noreply.github.com>
2023-09-12 17:10:56 +0300

    Revert "ryy"
    This reverts commit 89873980cd134ac8cea75603c1888c8c78b84fd8.

--- lib/enums.dart
+++ lib/enums.dart
@@ -4,21 +4,3 @@ enum sharedPrefrencesKeys {
   noBoarding,
   loginFirst,
 }
-
-enum CacheKeys {
-  id,
-  name,
-  email,
-  loggedIn,
-}
-
-enum FbCollection {
-  users,
-  categories,
-  products,
-  rates,
-  carts,
-  sliders,
-  notifications,
-  favorites,
-}
